b85b471b61dcecb52baa297b4380b321156ae81d
alexveliz0898 <alexveliz0898@gmail.com>
2020-11-17 15:20:53 -0500

    added axios for post/get

--- frontend-jukebox/src/App.js
+++ frontend-jukebox/src/App.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
 import {Link} from "react-router-dom"
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+    createRoom = ()=> {
+      console.log("This will create a new room woohoo")
+      /// Get input for Host Name and number of users needs
+      /// Create a random 6 digit room code
+      axios
+      .post('http://127.0.0.1:8000/api/home', {
+        roomID: "2020",
+        host: "CORONAVIRUS",
+        votes_to_skip: 1,
+      })
+      .then(response => (this.info = response.data))
+
+
+      console.log("Exiting room")
+    } 
+
+    handleSubmit = ()=>{
+      console.log("Entering Room")
+      // Request passcode and enter the correct room
+      // Make a GET request to enter that shit
+      // start adding songs!
+    }
 
  render(){ 
    return (
@@ -10,9 +34,9 @@ class App extends Component {
      
       <div className="options">
         <Link to="/Room">
-          <button>Make A Room</button>
+          <button onClick={this.createRoom}>Make A Room</button>
         </Link>  
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label>Join Room</label><br/>
           <input type="password"/>
         </form>
